65a49f2f0766f987d73fd36c57b791359e4cb3b2
Abdurrahman Köken <abdurrahman@akoken.com>
2016-08-16 23:39:17 +0300

    dependency resolution layer added.

---
+++ src/App.Infrastructure.DependencyResolution/LoggingModule.cs
@@ -0,0 +1,24 @@
+﻿using App.Infrastructure.Interfaces;
+using Ninject.Modules;
+using NLog;
+using NLog.Config;
+
+namespace App.Infrastructure.DependencyResolution
+{
+    public class LoggingModule : NinjectModule
+    {
+        public override void Load()
+        {
+            ILoggingService logger = GetLoggingService();
+            Bind<ILoggingService>().ToConstant(logger);
+        }
+
+        private ILoggingService GetLoggingService()
+        {
+            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("utc_date", typeof(UtcDateRenderer));
+            ConfigurationItemFactory.Default.LayoutRenderers.RegisterDefinition("web_variables", typeof(WebVariablesRenderer));
+            ILoggingService logger = (ILoggingService)LogManager.GetLogger("NLogLogger", typeof(LoggingService));
+            return logger;
+        }
+    }
+}
