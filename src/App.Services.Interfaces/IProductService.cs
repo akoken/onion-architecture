8b87a848ba4a642c8b7bbfc362d01ac4e9a5a1dd
Abdurrahman Köken <abdurrahman@akoken.com>
2016-08-18 22:41:56 +0300

    Product service added.

---
+++ src/App.Services.Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using App.Domain.Entities;
+using System.Collections.Generic;
+
+namespace App.Services.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<Category> GetCategories();
+        IEnumerable<Product> GetProducts(int? categoryId);
+    }
+}
---
+++ src/App.Services.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("App.Services.Interfaces")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.Services.Interfaces")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e071065-d5e2-4d05-b9d7-ca7ce90fcde6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
