40de87161c9d79f20e7a567446ebc9a610a13b4e
Abdurrahman Köken <abdurrahman@akoken.com>
2016-08-18 23:33:21 +0300

    caching mechanism added.

---
+++ src/App.Infrastructure.Caching/HttpContextCacheAdapter.cs
@@ -0,0 +1,25 @@
+﻿using App.Infrastructure.Interfaces;
+using System.Web;
+
+namespace App.Infrastructure.Caching
+{
+    public class HttpContextCacheAdapter : ICacheService
+    {
+        public void Remove(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+
+        public void Store(string key, object data)
+        {
+            HttpContext.Current.Cache.Insert(key, data);
+        }
+
+        public T Retrieve<T>(string storageKey) where T : class
+        {
+            T item = HttpContext.Current.Cache.Get(storageKey) as T;          
+
+            return item;
+        }
+    }
+}
---
+++ src/App.Infrastructure.Interfaces/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Infrastructure.Interfaces
+{
+    public interface ICacheService
+    {
+        void Remove(string key);
+        void Store(string key, object data);
+        T Retrieve<T>(string storageKey) where T : class;
+    }
+}
