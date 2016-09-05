90100f2732e04e6817c9fe9a2ed497b798f1466c
Abdurrahman Köken <abdurrahman@akoken.com>
2016-09-05 10:11:01 +0300

    .travis.yml file updated.

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,46 @@
 #!/usr/bin/env bash
+repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $repoFolder
 
-#exit if any command fails
-set -e
-
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
+koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
+if [ ! -z $KOREBUILD_ZIP ]; then
+    koreBuildZip=$KOREBUILD_ZIP
 fi
 
-dotnet restore
+buildFolder=".build"
+buildFile="$buildFolder/KoreBuild.sh"
 
-# Instead, run directly with mono for the full .net version 
-dotnet build ./src/OnionArchitecture.Core.Domain -c Release -f netcoreapp
+if test ! -d $buildFolder; then
+    echo "Downloading KoreBuild from $koreBuildZip"
+    
+    tempFolder="/tmp/KoreBuild-$(uuidgen)"    
+    mkdir $tempFolder
+    
+    localZipFile="$tempFolder/korebuild.zip"
+    
+    retries=6
+    until (wget -O $localZipFile $koreBuildZip 2>/dev/null || curl -o $localZipFile --location $koreBuildZip 2>/dev/null)
+    do
+        echo "Failed to download '$koreBuildZip'"
+        if [ "$retries" -le 0 ]; then
+            exit 1
+        fi
+        retries=$((retries - 1))
+        echo "Waiting 10 seconds before retrying. Retries left: $retries"
+        sleep 10s
+    done
+    
+    unzip -q -d $tempFolder $localZipFile
+  
+    mkdir $buildFolder
+    cp -r $tempFolder/**/build/** $buildFolder
+    
+    chmod +x $buildFile
+    
+    # Cleanup
+    if test ! -d $tempFolder; then
+        rm -rf $tempFolder  
+    fi
+fi
 
-revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
-
-dotnet pack ./src/OnionArchitecture.Core.Domain -c Release -o ./artifacts --version-suffix=$revision  
+$buildFile -r $repoFolder "$@"
